45754ccd7b0544ea714c45d9972880ef97829d26
NghiaPham139 <58319281+NghiaPham139@users.noreply.github.com>
2019-12-06 11:22:12 +0700

    update code SVM_HOG
    chinh sua lop nhan dien khuon mat

--- SVM_HOG/FaceReco.py
+++ SVM_HOG/FaceReco.py
@@ -8,7 +8,7 @@ import numpy as np
 link = "C:\\Users\\Admin\\desktop\\data\\encodingSVM.pickle"
 data = pickle.loads(open(link, "rb").read())
 
-clf = svm.SVC(gamma='scale')
+clf = svm.SVC(kernel = 'linear')
 clf.fit(data["encodings"],data["names"])
 
 cap = cv2.VideoCapture(0)
@@ -20,29 +20,15 @@ else:
 while (ret):
     ret, frame = cap.read()
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    rgb = imutils.resize(frame, width=400)
-    r = frame.shape[1] / float(rgb.shape[1])
-
     boxes = face_recognition.face_locations(rgb, model="hog")
     encodings = face_recognition.face_encodings(rgb, boxes)
-    names = []
-    for encoding in encodings:
-        matches = clf.predict(encodings)
-        if matches!=None:
-            name=matches
-        else:
-            name = "Unknown"
-        names.append(name)
-
+    names = clf.predict(encodings)
+    print (encodings)
     for ((top, right, bottom, left), name) in zip(boxes, names):
-        top = int(top * r)
-        right = int(right * r)
-        bottom = int(bottom * r)
-        left = int(left * r)
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
-        y = top - 15 if top - 15 > 15 else top + 15
-        cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0), 2)
+        cv2.putText(frame, str(name), (top, right + left), cv2.FONT_HERSHEY_SIMPLEX, 1.1, (0, 255, 0))
     cv2.imshow("Frame", frame)
     if cv2.waitKey(1) == 27:
         break
-    cv2.destroyAllWindows()
+
+cv2.destroyAllWindows()
