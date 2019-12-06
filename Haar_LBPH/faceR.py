e96ea105427b59b76b8d7229885a8fb9d99beb2c
NghiaPham139 <58319281+NghiaPham139@users.noreply.github.com>
2019-12-06 09:12:30 +0700

    Face Reco by Haar_LBPH
    nap code nhan dien khuon mat bang thuat toan haar_LBPH

---
+++ Haar_LBPH/faceR.py
@@ -0,0 +1,43 @@
+import os
+import numpy as np
+import cv2
+from PIL import Image
+import imutils.paths as paths
+import pickle
+# For face recognition we will the the LBPH Face Recognizer
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+path = "C:\\Users\\Admin\\desktop\\data\\Data\\"
+face_cascade = cv2.CascadeClassifier('C:\\Users\\Admin\\desktop\\data\\haarcascade_frontalface_default.xml')
+def getImagesWithID(path):
+    imagepaths = list(paths.list_images(path))
+    # print image_path
+    faceSamples = []
+    IDs = []
+    Names=[]
+    ID=0
+    for (i, imagePath) in enumerate(imagepaths):
+        # Read the image and convert to grayscale
+        PIL_img = Image.open(imagePath).convert('L')
+        gray = np.array(PIL_img,'uint8')
+        # Get the label of the image
+        Name= imagePath.split(os.path.sep)[-2]
+        faces=face_cascade.detectMultiScale(gray)
+        for (x, y, w, h) in faces:
+            # Detect the face in the image
+            faceSamples.append(gray[y: y + h, x: x + w])
+            IDs.append(ID)
+            Names.append(Name)
+            ID=ID+1
+        cv2.imshow("Adding faces for traning",gray)
+        cv2.waitKey(10)
+    return IDs, faceSamples,Names
+Ids,faces,names  = getImagesWithID(path)
+recognizer.train(faces, np.array(Ids))
+recognizer.save("C:\\Users\\Admin\\desktop\\data\\trainingdata.yml")
+data={"IDs":Ids,"Names":names}
+output = open("C:\\Users\\Admin\\desktop\\data\\Data.pickle", "wb")
+pickle.dump(data, output)
+output.close()
+cv2.destroyAllWindows()
+print (Ids)
+print (names)
---
+++ Haar_LBPH/face_D.py
@@ -0,0 +1,34 @@
+
+import numpy as np
+import cv2
+import os
+path = "C:\\Users\\Admin\\desktop\\data\\Data\\"
+face_cascade = cv2.CascadeClassifier('C:\\Users\\Admin\\desktop\\data\\haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
+id = raw_input('enter user id ')
+try:
+    # Create target Directory
+    os.mkdir(path+str(id))
+    print("Directory " , path+str(id),  " Created ")
+except Exception:
+    print("Directory " , path+str(id) ,  " already exists")
+sampleN=0;
+while 1:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    if faces is not None:
+        for (x,y,w,h) in faces:
+            sampleN=sampleN+1;
+            cv2.imwrite(path+str(id)+ "\\" +str(sampleN)+ ".jpg", gray[y:y+h, x:x+w])
+            cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+            cv2.waitKey(100)
+    cv2.imshow('img',img)
+    cv2.waitKey(1)
+    if sampleN > 10:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
